78809b414fcd217f21fd59c1042145e495a620af
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-07-02 19:59:37 +0300

    update

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"/>
+
 <header>
     <div class="header-background">
         <div class="header-content">
@@ -6,15 +9,19 @@
             </div>
             <div class="search-bar">
                 <input type="text" placeholder="Căutați răspunsuri">
-                <button type="submit"><img src="images/search_icon.png" alt="Search"></button>
+                <button type="submit"><span class="material-symbols-outlined"> search</span></button>
             </div>
             <div class="alegeri-button">
                 <a href="alegeri2024.php">Alegeri 2024</a>
             </div>
         </div>
         <div class="view-buttons">
-            <button class="grid-view"><img src="images/grid_icon.png" alt="Grid View"></button>
-            <button class="list-view"><img src="images/list_icon.png" alt="List View"></button>
+            <button class="grid-view"><span class="material-symbols-outlined">
+grid_view
+</span></button>
+            <button class="list-view">  <span class="material-symbols-outlined">
+menu
+</span></button>
         </div>
     </div>
 </header>
--- index.php
+++ index.php
@@ -5,33 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>How to: România</title>
     <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
 <?php include 'includes/header.php'; ?>
 
 <main>
     <section class="institutions">
-<!--        --><?php
-//        include 'includes/config.php';
-//
-//        $conn = get_db_connection($config);
-//
-//        $sql = "SELECT id, nume_institutie, src_logo, descriere FROM institutii";
-//        $result = $conn->query($sql);
-//
-//        if ($result->num_rows > 0) {
-//            while($row = $result->fetch_assoc()) {
-//                echo '<div class="institution">';
-//                echo '<img src="images/' . $row["src_logo"] . '" alt="' . $row["nume_institutie"] . '">';
-//                echo '<p>' . $row["nume_institutie"] . '</p>';
-//                echo '<a href="institutie.php?id=' . $row["id"] . '">Află mai multe</a>';
-//                echo '</div>';
-//            }
-//        } else {
-//            echo "0 results";
-//        }
-//        $conn->close();
-//        ?>
+        <?php
+        include 'includes/config.php';
+
+        $conn = get_db_connection($config);
+
+        $sql = "SELECT id, nume_institutie, src_logo, descriere FROM institutii";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo '<div class="institution">';
+                echo '<img src="images/' . $row["src_logo"] . '" alt="' . $row["nume_institutie"] . '">';
+                echo '<p>' . $row["nume_institutie"] . '</p>';
+                echo '<a href="institutie.php?id=' . $row["id"] . '">Află mai multe</a>';
+                echo '</div>';
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
     </section>
 </main>
 
